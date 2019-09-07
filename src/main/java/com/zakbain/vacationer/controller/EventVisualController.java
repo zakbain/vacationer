bfd8c20148f43985a4f9977dafef06b6f913dbe2
zakbain <zakbain@gmail.com>
2019-09-06 21:45:34 -0400

    Add ability to update and delete events

--- src/main/java/com/zakbain/vacationer/controller/EventVisualController.java
+++ src/main/java/com/zakbain/vacationer/controller/EventVisualController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -37,6 +38,33 @@ public class EventVisualController {
 
         event.setCompleted(false);
         eventRepository.save(event);
+        model.addAttribute("events", eventRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateEvent(@PathVariable("id") long id, Model model) {
+        Event event = eventRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        model.addAttribute("event", event);
+        return "updateEvent";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateEvent(@PathVariable("id") long id, @Valid Event event, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            event.setId(id);
+            return "updateEvent";
+        }
+
+        eventRepository.save(event);
+        model.addAttribute("events", eventRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteEvent(@PathVariable("id") long id, Model model) {
+        Event event  = eventRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid Event Id:" + id));
+        eventRepository.delete(event);
         model.addAttribute("events", eventRepository.findAll());
         return "index";
     }
